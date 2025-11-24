3b6c320f7d0a4503644f0faab57ea1d7f5295fc8
zayd <zaydkhan3@gmail.com>
2025-11-24 14:35:49 -0800

    chore: remove Discord bot implementation, documentation, and project-level files, and add .start-dev.pids.

--- discord-bot/src/commands/index.js
+++
@@ -1,7 +0,0 @@
-import { data as pushBookData, execute as pushBookExecute } from './pushBook.js';
-
-export const commands = [pushBookData];
-
-export const commandHandlers = {
-  [pushBookData.name]: pushBookExecute,
-};
